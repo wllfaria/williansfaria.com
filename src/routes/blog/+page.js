4bc7a9b2485c70b5b2c6988f48d1b80f028fe3c7
wiru <dev.willians.faria@gmail.com>
2024-07-27 17:39:40 -0300

    rewrite again innit?

--- src/routes/blog/+page.js
+++
@@ -1,8 +0,0 @@
-import '$lib/asyncOption.js';
-
-/** @type {import('./$types').PageLoad} */
-export async function load({ fetch }) {
-    const res = await fetch('/api/blog').map(v => v);
-    const posts = /** @type {Option<Post[]>} */ (await res.unwrap().json().map(v => v));
-    return { posts: posts.unwrap() };
-}
--- src/routes/series/+page.js
+++
@@ -1,8 +0,0 @@
-import '$lib/asyncOption.js';
-
-/** @type {import('./$types').PageLoad} */
-export async function load({ fetch }) {
-	const res = await fetch('/api/series').ok();
-	const series = /** @type {Option<Serie[]>} */ (await res.unwrap().json().ok());
-	return { series: series.unwrapOr([]) };
-}
