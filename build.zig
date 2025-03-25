fcf8e0c0a056436e5aa86d174538eee9562d1470
wiru <dev.willians.faria@gmail.com>
2025-03-24 22:22:46 -0300

    i rewrote it again boys

---
+++ build.zig
@@ -0,0 +1,13 @@
+const std = @import("std");
+const zine = @import("zine");
+
+pub fn build(b: *std.Build) !void {
+    zine.website(b, .{
+        .title = "Sample Website",
+        .host_url = "https://sample.com",
+        .content_dir_path = "content",
+        .layouts_dir_path = "layouts",
+        .assets_dir_path = "assets",
+        .static_assets = &.{},
+    });
+}
